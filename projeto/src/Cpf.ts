723085e259f2bfa3c356c76fc52eaff179a3b2bf
Iago <iago.pferreiravr@gmail.com>
2022-12-14 15:06:19 -0300

    feat(Projeto): Cpf
    Implementando classe de Cpf

---
+++ projeto/src/Cpf.ts
@@ -0,0 +1,50 @@
+export default class Cpf {
+  constructor(readonly value: string) {
+    if (!this.validate(value)) throw new Error('Cpf inválido')
+  }
+  
+  private validate(cpf: string) {
+    if (!cpf) return false;
+
+    cpf = this.cleanCpf(cpf);
+
+    if (!this.isValidLenght(cpf)) return false;
+
+    if(this.hasAllDigitsEqual(cpf)) return false;
+
+    const digit1 = this.calculateDigit(cpf, 10);
+    const digit2 = this.calculateDigit(cpf, 11);
+
+    const checkDigit = this.extractDigit(cpf);
+    const calculatedDigit = `${digit1}${digit2}`
+
+    return checkDigit === calculatedDigit;
+  }
+
+  private cleanCpf(cpf: string) {
+    return cpf.replace(/\D/g, "");
+  }
+  
+  private isValidLenght(cpf: string) {
+    return cpf.length === 11;
+  }
+  
+  private hasAllDigitsEqual(cpf: string) {
+    const [firstDigit] = cpf;
+    return cpf.split('').every((character) => character === firstDigit);
+  }
+  
+  private extractDigit(cpf: string): string {
+    return cpf.slice(9);
+  }
+  
+  private calculateDigit(cpf: string, factor: number) {
+    let total = 0;
+    for (const digit of cpf) {
+      if (factor > 1) total += Number(digit) * factor--;
+    }
+    const rest = total % 11;
+  
+    return rest < 2 ? 0 : 11 - rest;
+  }
+}
