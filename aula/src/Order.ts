1b2d504d6881002291e307e62704370338a2afa8
iago <iago.pferreiravr@gmail.com>
2024-02-23 13:39:25 -0300

    Add OrderItem functionality to Order class

--- aula/src/Order.ts
+++ aula/src/Order.ts
@@ -1,8 +1,24 @@
 import Cpf from './Cpf';
+import Item from './Item';
+import OrderItem from './OrderItem';
 
 export default class Order {
 	cpf: Cpf;
+	orderItems: OrderItem[];
+
 	constructor(cpf: string) {
 		this.cpf = new Cpf(cpf);
+		this.orderItems = [];
+	}
+
+	addItem(item: Item, quantity: number) {
+		this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
+	}
+
+	getTotal() {
+		return this.orderItems.reduce(
+			(acc, orderItem) => (acc += orderItem.price * orderItem.quantity),
+			0
+		);
 	}
 }
