8faf77753a903cee71b13d316b784b9e06132f02
iago <iago.pferreiravr@gmail.com>
2024-02-23 13:39:09 -0300

    Add test for creating an order with multiple items

--- aula/test/Order.test.ts
+++ aula/test/Order.test.ts
@@ -1,5 +1,17 @@
+import Item from '../src/Item';
 import Order from '../src/Order';
 
 test('Não deve criar um pedido com CPF inválido', () => {
 	expect(() => new Order('111.111.111-11')).toThrow(new Error('Invalid CPF'));
+});
+
+test('Deve criar um pedido com 3 itens', () => {
+	const order = new Order('529.982.247-25');
+	order.addItem(new Item(1, 'Guitarra', 1000), 1);
+	order.addItem(new Item(2, 'Amplificador', 5000), 1);
+	order.addItem(new Item(3, 'Cabo', 30), 3);
+
+	const total = order.getTotal();
+
+	expect(total).toBe(6090);
 });
