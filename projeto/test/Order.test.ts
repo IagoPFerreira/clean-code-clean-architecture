99842e0192fdd4f3bda6865f52fc19f7b07a2976
iago <iago.pferreiravr@gmail.com>
2024-02-26 20:10:47 -0300

    Add tests for calculating freight in Order class

--- projeto/test/Order.test.ts
+++ projeto/test/Order.test.ts
@@ -54,6 +54,46 @@ describe('Casos de sucesso', () => {
 
 		expect(total).toBe(4872);
 	});
+
+	test('Deve calcular o frete de um pedido com 1 item', () => {
+		const order = new Order('529.982.247-25');
+		order.addItem(
+			new Item(guitar.id, guitar.name, guitar.price, guitar.dimensions),
+			1
+		);
+		expect(order.getFreight()).toBe(30);
+	});
+
+	test('Deve calcular o frete de um pedido com 3 itens', () => {
+		const order = new Order('529.982.247-25');
+		order.addItem(
+			new Item(guitar.id, guitar.name, guitar.price, guitar.dimensions),
+			1
+		);
+		order.addItem(
+			new Item(
+				amplifier.id,
+				amplifier.name,
+				amplifier.price,
+				amplifier.dimensions
+			),
+			1
+		);
+		order.addItem(
+			new Item(cable.id, cable.name, cable.price, cable.dimensions),
+			3
+		);
+		expect(order.getFreight()).toBe(235);
+	});
+
+	test('Deve retornar o valor de frete mínimo', () => {
+		const order = new Order('529.982.247-25');
+		order.addItem(
+			new Item(cable.id, cable.name, cable.price, cable.dimensions),
+			3
+		);
+		expect(order.getFreight()).toBe(10);
+	});
 });
 
 describe('Casos de falha', () => {
