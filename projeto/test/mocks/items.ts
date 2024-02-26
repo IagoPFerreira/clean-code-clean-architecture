ce4b21485013b6006ac0d0c8e88aba83146e5253
iago <iago.pferreiravr@gmail.com>
2024-02-26 19:37:17 -0300

    Add mock items for testing

---
+++ projeto/test/mocks/items.ts
@@ -0,0 +1,33 @@
+export const amplifier = {
+	id: 2,
+	name: 'Amplificador',
+	price: 5000,
+	dimensions: {
+		height: 10,
+		width: 30,
+		length: 100,
+		weight: 20,
+	},
+};
+export const guitar = {
+	id: 1,
+	name: 'Guitarra',
+	price: 1000,
+	dimensions: {
+		height: 10,
+		width: 30,
+		length: 100,
+		weight: 2,
+	},
+};
+export const cable = {
+	id: 3,
+	name: 'Cabo',
+	price: 30,
+	dimensions: {
+		height: 10,
+		width: 30,
+		length: 100,
+		weight: 0.5,
+	},
+};
