9cb51549a18a8468c42589154c2f79d81e492424
iago <iago.pferreiravr@gmail.com>
2024-02-26 19:36:29 -0300

    Add dimensions validation to Item constructor

--- projeto/src/Item.ts
+++ projeto/src/Item.ts
@@ -2,6 +2,21 @@ export default class Item {
 	constructor(
 		readonly idItem: number,
 		readonly description: string,
-		readonly price: number
-	) {}
+		readonly price: number,
+		readonly dimensions: {
+			height: number;
+			length: number;
+			width: number;
+			weight: number;
+		}
+	) {
+		if (
+			dimensions.height <= 0 ||
+			dimensions.length <= 0 ||
+			dimensions.width <= 0 ||
+			dimensions.weight <= 0
+		) {
+			throw new Error('Invalid dimensions');
+		}
+	}
 }
