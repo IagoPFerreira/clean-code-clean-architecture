7e9aac553aae09ee519a1bf5b105c8d19d5548bb
iago <iago.pferreiravr@gmail.com>
2024-02-26 20:11:19 -0300

    Add density and volume properties to OrderItem class

--- projeto/src/OrderItem.ts
+++ projeto/src/OrderItem.ts
@@ -2,7 +2,9 @@ export default class OrderItem {
 	constructor(
 		readonly idItem: number,
 		readonly price: number,
-		readonly quantity: number
+		readonly quantity: number,
+		readonly density: number,
+		readonly volume: number
 	) {}
 
 	getTotal() {
