11a58de450fe6f49e9af09b1dc7fd9aeed63f8fc
iago <iago.pferreiravr@gmail.com>
2024-02-26 19:36:39 -0300

    Refactor order creation and item addition in Order.test.ts

--- projeto/test/Order.test.ts
+++ projeto/test/Order.test.ts
@@ -1,6 +1,7 @@
 import Coupon from '../src/Coupon';
 import Item from '../src/Item';
 import Order from '../src/Order';
+import { amplifier, cable, guitar } from './mocks/items';
 
 test('Não deve criar um pedido com CPF inválido', () => {
 	expect(() => new Order('111.111.111-11')).toThrow(new Error('Invalid CPF'));
@@ -8,9 +9,23 @@ test('Não deve criar um pedido com CPF inválido', () => {
 
 test('Deve criar um pedido com 3 itens', () => {
 	const order = new Order('529.982.247-25');
-	order.addItem(new Item(1, 'Guitarra', 1000), 1);
-	order.addItem(new Item(2, 'Amplificador', 5000), 1);
-	order.addItem(new Item(3, 'Cabo', 30), 3);
+	order.addItem(
+		new Item(guitar.id, guitar.name, guitar.price, guitar.dimensions),
+		1
+	);
+	order.addItem(
+		new Item(
+			amplifier.id,
+			amplifier.name,
+			amplifier.price,
+			amplifier.dimensions
+		),
+		1
+	);
+	order.addItem(
+		new Item(cable.id, cable.name, cable.price, cable.dimensions),
+		3
+	);
 
 	const total = order.getTotal();
 
@@ -19,9 +34,88 @@ test('Deve criar um pedido com 3 itens', () => {
 
 test('Deve criar um pedido com 3 itens com cupom de desconto', () => {
 	const order = new Order('529.982.247-25');
-	order.addItem(new Item(1, 'Guitarra', 1000), 1);
-	order.addItem(new Item(2, 'Amplificador', 5000), 1);
-	order.addItem(new Item(3, 'Cabo', 30), 3);
+	order.addItem(
+		new Item(guitar.id, guitar.name, guitar.price, guitar.dimensions),
+		1
+	);
+	order.addItem(
+		new Item(
+			amplifier.id,
+			amplifier.name,
+			amplifier.price,
+			amplifier.dimensions
+		),
+		1
+	);
+	order.addItem(
+		new Item(cable.id, cable.name, cable.price, cable.dimensions),
+		3
+	);
+	order.addCoupon(new Coupon('VALE20', 20));
+
+	const total = order.getTotal();
+
+	expect(total).toBe(4872);
+});
+
+test('Não deve criar um pedido com algum item negativo', () => {
+	const order = new Order('529.982.247-25');
+	order.addItem(
+		new Item(guitar.id, guitar.name, guitar.price, guitar.dimensions),
+		1
+	);
+	expect(() =>
+		order.addItem(
+			new Item(
+				amplifier.id,
+				amplifier.name,
+				amplifier.price,
+				amplifier.dimensions
+			),
+			-1
+		)
+	).toThrow(new Error('Invalid quantity'));
+	order.addItem(
+		new Item(cable.id, cable.name, cable.price, cable.dimensions),
+		3
+	);
+	order.addCoupon(new Coupon('VALE20', 20));
+
+	const total = order.getTotal();
+
+	expect(total).toBe(872);
+});
+
+test('Não deve criar um pedido com algum item repetido', () => {
+	const order = new Order('529.982.247-25');
+	order.addItem(
+		new Item(guitar.id, guitar.name, guitar.price, guitar.dimensions),
+		1
+	);
+	order.addItem(
+		new Item(
+			amplifier.id,
+			amplifier.name,
+			amplifier.price,
+			amplifier.dimensions
+		),
+		1
+	);
+	expect(() =>
+		order.addItem(
+			new Item(
+				amplifier.id,
+				amplifier.name,
+				amplifier.price,
+				amplifier.dimensions
+			),
+			2
+		)
+	).toThrow(new Error('Already added item'));
+	order.addItem(
+		new Item(cable.id, cable.name, cable.price, cable.dimensions),
+		3
+	);
 	order.addCoupon(new Coupon('VALE20', 20));
 
 	const total = order.getTotal();
