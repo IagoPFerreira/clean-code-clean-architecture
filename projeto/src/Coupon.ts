edd7f55249a080b5084cb3daf96682f0b51bc4f1
iago <iago.pferreiravr@gmail.com>
2024-02-26 19:35:56 -0300

    Refactor Coupon class constructor to include expiration date check

--- projeto/src/Coupon.ts
+++ projeto/src/Coupon.ts
@@ -1,5 +1,13 @@
 export default class Coupon {
-	constructor(readonly code: string, readonly percentage: number) {}
+	constructor(
+		readonly code: string,
+		readonly percentage: number,
+		readonly expiration?: Date
+	) {
+		if (expiration && expiration.getTime() < Date.now()) {
+			throw new Error('Expired coupon');
+		}
+	}
 
 	calculateDiscount(total: number) {
 		return (total * this.percentage) / 100;
