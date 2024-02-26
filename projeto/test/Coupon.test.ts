a1b6cbc5fc6192391de4311a8fb9c66b4b70c969
iago <iago.pferreiravr@gmail.com>
2024-02-26 19:57:01 -0300

    Add test for calculating discount

--- projeto/test/Coupon.test.ts
+++ projeto/test/Coupon.test.ts
@@ -5,6 +5,11 @@ describe('Casos de sucesso', () => {
 		const coupon = new Coupon('VALE20', 20, new Date('2030-10-10'));
 		expect(coupon).toBeDefined();
 	});
+
+	test('Deve retornar o valor com desconto', () => {
+		const coupon = new Coupon('VALE20', 20, new Date('2030-10-10'));
+		expect(coupon.calculateDiscount(500)).toBe(100);
+	});
 });
 
 describe('Casos de falha', () => {
